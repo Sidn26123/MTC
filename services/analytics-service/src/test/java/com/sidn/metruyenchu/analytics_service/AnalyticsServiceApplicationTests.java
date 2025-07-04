eb4c202d9881626e4b8e78227bf5cc5f81004ea4
sidn <sidaconbabanbanhda365@gmail.com>
2025-07-04 16:22:16 +0700

    feat: add analytic service

---
+++ services/analytics-service/src/test/java/com/sidn/metruyenchu/analytics_service/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidn.metruyenchu.analytics_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
