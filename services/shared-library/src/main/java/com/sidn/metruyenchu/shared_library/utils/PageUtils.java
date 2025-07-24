f730e44c02bb5d1a77292068514bb08e14a27394
sidn <sidaconbabanbanhda365@gmail.com>
2025-07-24 13:16:28 +0700

    chore: add page utils func

--- services/shared-library/src/main/java/com/sidn/metruyenchu/shared_library/utils/PageUtils.java
+++ services/shared-library/src/main/java/com/sidn/metruyenchu/shared_library/utils/PageUtils.java
@@ -2,11 +2,15 @@ package com.sidn.metruyenchu.shared_library.utils;
 
 import com.sidn.metruyenchu.shared_library.dto.BaseFilterRequest;
 import com.sidn.metruyenchu.shared_library.dto.PageResponse;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.util.Arrays;
 import java.util.function.Function;
 
 public class PageUtils {
@@ -45,5 +49,40 @@ public class PageUtils {
         );
 
         return pageable;
+    }
+
+    public static <T> T mapBaseFilter(BaseFilterRequest from, T to) {
+        if (from == null || to == null) return to;
+
+        try {
+            // Dùng reflection để gán giá trị cho các field chung
+            BeanUtils.copyProperties(from, to, getNullPropertyNames(from));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to copy base filter properties", e);
+        }
+
+        return to;
+    }
+
+    public static BaseFilterRequest toBaseFilter(Object from) {
+        if (from == null) return  BaseFilterRequest.builder().build(); // Trả về filter mặc định
+
+        BaseFilterRequest filter = BaseFilterRequest.builder().build(); // Tạo với default value
+
+        try {
+            BeanUtils.copyProperties(from, filter, getNullPropertyNames(from));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to map entity to BaseFilterRequest", e);
+        }
+
+        return filter;
+    }
+    // Loại bỏ các property null khi copy để tránh overwrite dữ liệu đã có
+    private static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        return Arrays.stream(src.getPropertyDescriptors())
+                .map(pd -> pd.getName())
+                .filter(propertyName -> src.getPropertyValue(propertyName) == null)
+                .toArray(String[]::new);
     }
 }
